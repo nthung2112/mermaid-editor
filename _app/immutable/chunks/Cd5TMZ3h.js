ebdecc03d3d739ffb3012930f30e6c1ead6cf217
Hung Nguyen <nthung2112@gmail.com>
2025-10-24 10:15:32 +0700

    Update link

---
+++ _app/immutable/chunks/Cd5TMZ3h.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_xn965u?.base??"",a=globalThis.__sveltekit_xn965u?.assets??s;export{a,s as b};
--- _app/immutable/chunks/D_G8tMgj.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1c046hk?.base??"",a=globalThis.__sveltekit_1c046hk?.assets??s;export{a,s as b};
